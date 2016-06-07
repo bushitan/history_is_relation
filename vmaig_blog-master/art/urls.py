660d2e4cc1ac9bf42c8160b9f93e55a9a0b43d75
bushitan <373514952@qq.com>
2016-06-07 15:22:21 +0800

    v1.1.11.14  art/static/img 增加测试图片

--- vmaig_blog-master/art/urls.py
+++ vmaig_blog-master/art/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from art.views import *
 urlpatterns = [
 
-    url(r'^art/main$', MainView.as_view()),
+   url(r'^art/main$', MainView.as_view()),
    url(r'^art/pixel_tool$', PixelToolView.as_view()),
    url(r'^art/pixel_tool/(?P<filename>\w+)$', PixelToolView.as_view()),
    url(r'^art/upload$', UploadView.as_view()),
